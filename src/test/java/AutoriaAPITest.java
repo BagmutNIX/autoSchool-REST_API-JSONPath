a26c4afb761fb30110bc00f3857acf303783b2e0
Inna <innasamus@gmail.com>
2020-10-17 17:36:22 +0300

    finished with REST Assured

--- src/test/java/AutoriaAPITest.java
+++ src/test/java/AutoriaAPITest.java
@@ -1,27 +1,34 @@
 import io.restassured.RestAssured;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class AutoriaAPITest {
 
-@Test
-    public void autoriaAPITest() {
-    RestAssured.given()
-            .baseUri("https://auto.ria.com")
-            .basePath("/auto_mitsubishi_lancer_21990745.html")
-            .when().get()
-            .then().extract().response()
-            .prettyPrint();
-}
+    @Test(dataProvider = "url")
+    public void autoriaAPITest(String url) {
+        RestAssured.given()
+                .baseUri("https://auto.ria.com")
+                .basePath(url)
+                .header("User-Agent", "Jmeter")
+                .when().get()
+                .then()
+                .statusCode(200)
+                .contentType("text/html")
+                .header("Content-Encoding", "gzip");
+    }
+
+    @DataProvider(name = "url")
+    public Object[] url() {
+        return new String[]{
+                "/auto_mitsubishi_lancer_21990745.html",
+                "/auto_toyota_camry_22101863.html",
+                "/auto_bmw_520_21996828.html",
+                "/auto_nissan_leaf_22104675.html",
+                "/auto_nissan_leaf_22104647.html",
+                "/auto_mercedes_benz_e_220_21994701.html",
+                "/auto_lexus_nx_200_22083607.html"
+        };
+    }
 }
 
-
-/*
-        https://auto.ria.com/auto_mitsubishi_lancer_21990745.html
-        https://auto.ria.com/auto_toyota_camry_22101863.html
-        https://auto.ria.com/auto_bmw_520_21996828.html
-        https://auto.ria.com/auto_nissan_leaf_22104675.html
-        https://auto.ria.com/auto_nissan_leaf_22104647.html
-        https://auto.ria.com/auto_mercedes_benz_e_220_21994701.html
-        https://auto.ria.com/auto_lexus_nx_200_22083607.html
-*/
 
